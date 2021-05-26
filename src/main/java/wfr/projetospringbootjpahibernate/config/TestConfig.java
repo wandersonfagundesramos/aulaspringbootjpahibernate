07d50c4662d8fb40907c0675271e49c7a23e4f51
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-05-26 18:19:16 -0300

    Category

--- src/main/java/wfr/projetospringbootjpahibernate/config/TestConfig.java
+++ src/main/java/wfr/projetospringbootjpahibernate/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import wfr.projetospringbootjpahibernate.entities.Category;
 import wfr.projetospringbootjpahibernate.entities.Order;
 import wfr.projetospringbootjpahibernate.entities.User;
 import wfr.projetospringbootjpahibernate.entities.enums.OrderStatus;
+import wfr.projetospringbootjpahibernate.repositories.CategoryRepository;
 import wfr.projetospringbootjpahibernate.repositories.OrderRepository;
 import wfr.projetospringbootjpahibernate.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Mateus Gabriel", "mateus@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Mariana Gabriele", "marianax@gmail.com", "977777777", "123456"); 
