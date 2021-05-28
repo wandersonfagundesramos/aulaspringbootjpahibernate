d1844ca65d6174a21be96a17aa41fa1e038de3f4
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-05-28 17:51:15 -0300

    OrderItem, many-to-many association with extra attributes - PART 1

--- src/main/java/wfr/projetospringbootjpahibernate/config/TestConfig.java
+++ src/main/java/wfr/projetospringbootjpahibernate/config/TestConfig.java
@@ -47,7 +47,6 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 
-		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2,p3, p4, p5));
 		
