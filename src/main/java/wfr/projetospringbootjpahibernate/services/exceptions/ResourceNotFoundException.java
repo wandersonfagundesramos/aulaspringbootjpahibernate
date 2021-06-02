199ef28d49a5b7b7e7b844dd022c1bff84216edd
Wanderson Fagundes <wandersonfagundesramos@gmail.com>
2021-06-02 14:28:39 -0300

    Exception handling - findById

---
+++ src/main/java/wfr/projetospringbootjpahibernate/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package wfr.projetospringbootjpahibernate.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException () {
+		
+	}
+	
+	public ResourceNotFoundException (Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
